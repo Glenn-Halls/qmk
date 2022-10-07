37671a0f80c1b4253a867422663b08f1276d4671
Glenn Halls <gjhalls@gmail.com>
2022-10-07 14:50:38 +1100

    Update layer 1

--- keebio/iris/keymaps/via/keymap.c
+++ keebio/iris/keymaps/via/keymap.c
@@ -64,13 +64,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_PLUS, KC_PSCR,  KC_INS,  KC_UP,   KC_PGUP, KC_ENT,                             KC_BSLS, KC_P7,   KC_P8,   KC_P9,   KC_EQL,  KC_TILD,
+     KC_PLUS, KC_PSCR,  KC_INS,  KC_UP,   KC_PGUP, KC_ENT,                            KC_BSLS,  KC_P7,   KC_P8,   KC_P9,   KC_EQL,  KC_TILD,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_UNDS,  KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_LCBR,                           KC_ASTR,  KC_P4,   KC_P5,   KC_P6,   KC_COLN, KC_DQUO,
+     KC_UNDS,  KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                           KC_ASTR,  KC_P4,   KC_P5,   KC_P6,   KC_COLN, KC_DQUO,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCBR,  KC_EXLM, KC_HOME, KC_END,  KC_PGDN, KC_DLR, KC_BSPC,          _______, KC_PIPE,  KC_P1,   KC_P2,   KC_P3,   KC_QUES, KC_RCBR,
+     KC_LCBR,  KC_EXLM, KC_HOME, KC_END,  KC_PGDN, KC_AMPR, KC_BSPC,         _______, KC_PIPE,  KC_P1,   KC_P2,   KC_P3,   KC_QUES, KC_RCBR,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   KC_SPC,   _______,  KC_P0
+                                    _______, _______, _______,                   KC_SPC,   _______, KC_P0
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
