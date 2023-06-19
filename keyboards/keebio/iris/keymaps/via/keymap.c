ba4175cfc0b85adefae29a51bde1889acab71aff
Glenn Halls <gjhalls@gmail.com>
2023-06-19 15:21:17 +1000

    Update: Jetpack macros

--- keyboards/keebio/iris/keymaps/via/keymap.c
+++ keyboards/keebio/iris/keymaps/via/keymap.c
@@ -97,23 +97,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         break;
 
     case MCR_04:
-    // Java: print
+    // Jetpack: parent modifier
         if (record->event.pressed) {
-            SEND_STRING("System.out.print(\"\");" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT));
+            SEND_STRING("modifier: Modifier = Modifier");
         }
         break;
 
     case MCR_05:
-    //  Java: println
+    //  Jetpack: child modifier
         if (record->event.pressed) {
-            SEND_STRING("System.out.println(\"\");" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT));
+            SEND_STRING("modifier = Modifier");
         }
         break;
 
     case MCR_06:
-    // Java: printf
+    // Jetpack Compose tag
         if (record->event.pressed) {
-            SEND_STRING("System.out.printf(\"\");" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT));
+            SEND_STRING("@Composable" SS_TAP(X_ENT));
         }
         break;
     
@@ -160,13 +160,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_PLUS, KC_PSCR,  KC_PGUP, KC_UP,   KC_INS,  KC_ENT,                            KC_BSLS,  KC_7,    KC_8,    KC_9,    KC_EQL,  KC_TILD,
+     KC_PLUS, KC_PSCR,  KC_PGUP, KC_UP,   KC_INS,  KC_ENT,                            KC_BSLS,  KC_7,    KC_8,    KC_9,    KC_EQL,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_UNDS,  KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                           KC_ASTR,  KC_4,    KC_5,    KC_6,    KC_COLN, KC_DQUO,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCBR,  KC_EXLM, KC_PGDN, KC_HOME, KC_END,  KC_AMPR, KC_BSPC,         _______, KC_PIPE,  KC_1,    KC_2,    KC_3,    KC_0,    KC_RCBR,
+     KC_LCBR,  KC_EXLM, KC_PGDN, KC_HOME, KC_END,  KC_AMPR, KC_BSPC,         _______, KC_PIPE,  KC_1,    KC_2,    KC_3,    KC_SLSH, KC_RCBR,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   KC_SPC,   _______, KC_F11
+                                    _______, _______, _______,                   KC_SPC,   _______, KC_0
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
